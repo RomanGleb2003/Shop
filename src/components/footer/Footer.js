7c2e2113987d9a03a4f2533cda85febbbe56acce
unknown <roman.gleb.03@gmail.com>
2023-03-03 22:51:06 +0200

    commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export function Footer() {
+    return (
+        <footer className='bg-black text-white'>
+            <div className='max-w-[1240px] mx-auto py-2 text-sm sm:text-base'>
+                <div className='flex justify-center flex-col items-center'>
+                    <p className='text-center'>© Copyright 2022 React Shop | All Rights Reserved</p>
+                    <p>
+                        Created by Roman Gleb
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
