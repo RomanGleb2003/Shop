b6c5e1968d7757b2e3fc4632a9e886377e828fc8
unknown <roman.gleb.03@gmail.com>
2023-03-04 14:23:57 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react"
 import {Context} from "./context/context"
-import {Route, Routes} from "react-router-dom"
+import {Navigate, Route, Routes} from "react-router-dom"
 import {HomePage} from "./pages/HomePage"
 import {ProductOverView} from "./pages/ProductOverView"
 import {Header} from "./components/header/Header"
 import {Footer} from "./components/footer/Footer"
 import {BtnScroll} from "./components/btn-scroll/BtnScroll"
+import * as PropTypes from "prop-types";
+
+function Redirect(props) {
+    return null;
+}
+
+Redirect.propTypes = {
+    from: PropTypes.string,
+    to: PropTypes.string
+};
 
 export default function App() {
     const productId = {id: 0}
@@ -15,8 +25,9 @@ export default function App() {
             <Context.Provider value={productId}>
                 <Header />
                 <Routes>
-                    <Route path='/' element={ <HomePage /> }/>
+                    <Route path='/home' element={ <HomePage /> }/>
                     <Route path='/products/:id' element={ <ProductOverView /> }/>
+                    <Route path="*" element={<Navigate to="/home" replace />} />
                 </Routes>
                 <BtnScroll />
                 <Footer/>
